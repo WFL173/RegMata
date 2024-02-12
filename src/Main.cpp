e42409fe0df067ef79935db8d281b6a0d067f97b
Waffle38 <57353355+Waffle38@users.noreply.github.com>
2024-02-12 15:34:35 +0300

    Removed unnecassary operations in main.

--- src/Main.cpp
+++ src/Main.cpp
@@ -10,12 +10,6 @@ int main ()
 {
     _CrtSetDbgFlag(_CRTDBG_LEAK_CHECK_DF | _CRTDBG_ALLOC_MEM_DF);
 
-    ArrayList<int> list = {0};
-    list.Add(1);
-    list.Add(2);
-    list.Add(3);
-    list.Add(4);
-
     char regex[] = "aaba*";
 
     char *postfix = Regex2Postfix(regex);
